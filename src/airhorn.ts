3432156866f082c5b7f79e95e046a5ac374ae16f
Jared Wray <me@jaredwray.com>
2021-04-08 16:40:14 -0700

    Adding in logging

--- src/airhorn.ts
+++ src/airhorn.ts
@@ -1,6 +1,15 @@
-export class Airhorn {
-    constructor() {
-        console.log("This is an init project. DO NOT USE. Please follow along at https://github.com/jaredwray/airhorn");
+import { Config } from "./config";
+import { Template } from "./template";
+import * as Logger from "./logger";
 
-    }  
+const logger = Logger.create();
+
+export class Airhorn {
+
+    config = new Config();
+    templates = new Array<Template>();
+
+    constructor() {
+        logger.error("This is an init project. DO NOT USE. Please follow along at https://github.com/jaredwray/airhorn");
+    }
 }
