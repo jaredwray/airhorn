266dfc05a7f11dc03e4ed83520689cc67dfd9fa8
Jared Wray <jaredwray@gmail.com>
2021-05-21 21:37:46 -0700

    Update airhorn.ts

--- src/airhorn.ts
+++ src/airhorn.ts
@@ -12,4 +12,11 @@ export class Airhorn {
     constructor() {
         logger.error("This is an init project. DO NOT USE. Please follow along at https://github.com/jaredwray/airhorn");
     }
+
+    //templates
+    async getTemplate(name:string): Promise<Template> {
+        let result = new Template();
+
+        return result;
+    }
 }
