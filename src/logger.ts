758adfb887cf7c34cd23e34a61ebeaf0f31226da
Jared Wray <me@jaredwray.com>
2021-10-13 09:50:24 -0700

    added in node:process

--- src/logger.ts
+++ src/logger.ts
@@ -1,9 +1,11 @@
-import * as winston from "winston";
+import * as process from 'node:process';
+import * as winston from 'winston';
 
 export function create() {
-    let log = winston.createLogger({ transports: [ new winston.transports.Console() ]});
-    if(process.env.NODE_ENV === "test") {
-        log.silent = true;
-    }
-    return log;
+	const log = winston.createLogger({transports: [new winston.transports.Console()]});
+	if (process.env.NODE_ENV === 'test') {
+		log.silent = true;
+	}
+
+	return log;
 }
