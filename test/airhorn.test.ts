d92cbe3b30e7d1ebb8ee085be8b1920874b27b2d
Jared Wray <me@jaredwray.com>
2021-10-26 13:38:52 -0700

    updating to es2019 compiler

--- test/airhorn.test.ts
+++ test/airhorn.test.ts
@@ -1,5 +1,12 @@
 import {Airhorn} from '../src/airhorn';
+import {Config} from '../src/config';
 
-test('Test Init Error', () => {
+test('Airhorn Init', () => {
 	expect(new Airhorn()).toEqual(new Airhorn());
+});
+
+test('Get Templates', () => {
+	const airhorn = new Airhorn();
+
+	expect(airhorn.templates.config).toEqual(new Config());
 });
