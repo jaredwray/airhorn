9751f2d68dcffda6fd78d91cda2a53d014775230
Jared Wray <me@jaredwray.com>
2021-11-08 15:35:49 -0800

    updating airhorn with send

--- test/airhorn.test.ts
+++ test/airhorn.test.ts
@@ -1,5 +1,7 @@
 import {Airhorn} from '../src/airhorn';
 import {Config} from '../src/config';
+import {ProviderType} from '../src/provider-type';
+import {TestingData} from './testing-data';
 
 test('Airhorn - Init', () => {
 	expect(new Airhorn()).toEqual(new Airhorn());
@@ -24,4 +26,23 @@ test('Airhorn - Options Validated in Config', () => {
 	const airhorn = new Airhorn(options);
 
 	expect(airhorn.config.templatePath).toEqual(options.templatePath);
+});
+
+test('Airhorn - Get Provider By Type', () => {
+	const options = {
+		templatePath: './test/templates',
+	};
+	const airhorn = new Airhorn(options);
+
+	expect(airhorn.providers.getProviderByType(ProviderType.WEBHOOK).length).toEqual(1);
+});
+
+test('Airhorn - Send WebHook', async () => {
+	const options = {
+		templatePath: './test/templates',
+	};
+	const airhorn = new Airhorn(options);
+	const userData = new TestingData();
+
+	expect(await airhorn.send('cool-multi-lingual', ProviderType.WEBHOOK, userData.users[0])).toEqual(true);
 });
