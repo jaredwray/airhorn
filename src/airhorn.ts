14afc185108db7058524956da58288c33f6dafe0
Jared Wray <me@jaredwray.com>
2021-11-08 09:46:00 -0800

    moving airhorn config to be set and get

--- src/airhorn.ts
+++ src/airhorn.ts
@@ -3,21 +3,17 @@ import {TemplateService} from './template-service';
 import {ProviderService} from './provider-service';
 
 export class Airhorn {
-	private readonly _config = new Config();
+	config = new Config();
 
 	private readonly _templateService = new TemplateService();
 	private readonly _providerService = new ProviderService();
 
 	constructor(options?: any) {
 		if (options) {
-			this._config = new Config(options);
+			this.config = new Config(options);
 			this._templateService = new TemplateService(options);
 			this._providerService = new ProviderService(options);
 		}
-	}
-
-	public get config(): Config {
-		return this._config;
 	}
 
 	public get templates(): TemplateService {
