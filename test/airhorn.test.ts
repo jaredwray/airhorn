08400917ed813389e300a18b24e78249ae6e9c89
Jared Wray <me@jaredwray.com>
2021-11-07 19:52:06 -0800

    testing get providers

--- test/airhorn.test.ts
+++ test/airhorn.test.ts
@@ -1,14 +1,20 @@
 import {Airhorn} from '../src/airhorn';
 import {Config} from '../src/config';
 
-test('Airhorn Init', () => {
+test('Airhorn - Init', () => {
 	expect(new Airhorn()).toEqual(new Airhorn());
 });
 
-test('Get Templates', () => {
+test('Airhorn - Get Templates', () => {
 	const airhorn = new Airhorn();
 
 	expect(airhorn.templates.config).toEqual(new Config());
+});
+
+test('Airhorn - Get Providers', () => {
+	const airhorn = new Airhorn();
+
+	expect(airhorn.providers.config).toEqual(new Config());
 });
 
 test('Airhorn - Options Validated in Config', () => {
