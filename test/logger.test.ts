62dfe60c6d51776ef621e0351f3d31476c7b1ed9
Jared Wray <me@jaredwray.com>
2021-11-28 12:26:00 -0800

    adding test env in title

--- test/logger.test.ts
+++ test/logger.test.ts
@@ -1,13 +1,13 @@
 /* eslint-disable node/prefer-global/process */
 import * as Logger from '../src/logger';
 
-test('Logger - silence', () => {
+test('Logger - silence with foo env', () => {
 	process.env.NODE_ENV = 'foo';
 	const logger = Logger.create();
 	expect(logger.silent).toEqual(undefined);
 });
 
-test('Logger - silence', () => {
+test('Logger - silence with test env', () => {
 	process.env.NODE_ENV = 'test';
 	const logger = Logger.create();
 	expect(logger.silent).toEqual(true);
