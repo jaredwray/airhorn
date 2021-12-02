dfd91653bc2c087682f5e3e245ec1d7bcb58f933
Jared Wray <me@jaredwray.com>
2021-12-01 22:12:53 -0800

    adding in properties for subject

--- src/template-text.ts
+++ src/template-text.ts
@@ -1,10 +1,11 @@
-
+/* eslint-disable @typescript-eslint/no-unsafe-call */
 export class TemplateText {
 	text = '';
 	format = '';
 	language = '';
+	properties = new Map<string, string>();
 
-	constructor(text?: string, format?: string, language?: string) {
+	constructor(text?: string, format?: string, language?: string, properties?: any) {
 		if (text !== undefined) {
 			this.text = text;
 		}
@@ -15,6 +16,14 @@ export class TemplateText {
 
 		if (language !== undefined) {
 			this.language = language;
+		}
+
+		if (properties !== undefined) {
+			for (const key in properties) {
+				if (properties[key] !== undefined) {
+					this.properties.set(key, properties[key].toString());
+				}
+			}
 		}
 	}
 }
