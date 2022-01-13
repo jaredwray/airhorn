338038510a32c8793acf9d13869acc37fa7e6ad5
Jared Wray <me@jaredwray.com>
2022-01-13 11:04:00 -0800

    moved to a single getValue for all parsing

--- src/config.ts
+++ src/config.ts
@@ -8,6 +8,7 @@ export class Config {
 	TWILIO_SMS_AUTH_TOKEN = '';
 	TWILIO_SENDGRID_API_KEY = '';
 	AWS_SES_REGION = '';
+	AWS_SMS_REGION = '';
 
 	constructor(options?: any) {
 		if (options) {
@@ -21,40 +22,38 @@ export class Config {
 	}
 
 	parse(options: any) {
-		if (options.TEMPLATE_PATH) {
-			this.TEMPLATE_PATH = this.cleanValue(options.TEMPLATE_PATH);
-		}
+		this.TEMPLATE_PATH = this.getValueFromOptionsOrEnv(options, 'TEMPLATE_PATH', this.TEMPLATE_PATH);
 
-		if (options.DEFAULT_TEMPLATE_LANGUAGE) {
-			this.DEFAULT_TEMPLATE_LANGUAGE = this.cleanValue(options.DEFAULT_TEMPLATE_LANGUAGE);
-		}
+		this.DEFAULT_TEMPLATE_LANGUAGE = this.getValueFromOptionsOrEnv(options, 'DEFAULT_TEMPLATE_LANGUAGE', this.DEFAULT_TEMPLATE_LANGUAGE);
 
-		if (options.TWILIO_SMS_ACCOUNT_SID) {
-			this.TWILIO_SMS_ACCOUNT_SID = this.cleanValue(options.TWILIO_SMS_ACCOUNT_SID);
-		} else if (process.env.TWILIO_SMS_ACCOUNT_SID) {
-			this.TWILIO_SMS_ACCOUNT_SID = process.env.TWILIO_SMS_ACCOUNT_SID;
-		}
+		this.TWILIO_SMS_ACCOUNT_SID = this.getValueFromOptionsOrEnv(options, 'TWILIO_SMS_ACCOUNT_SID', this.TWILIO_SMS_ACCOUNT_SID);
 
-		if (options.TWILIO_SMS_AUTH_TOKEN) {
-			this.TWILIO_SMS_AUTH_TOKEN = this.cleanValue(options.TWILIO_SMS_AUTH_TOKEN);
-		} else if (process.env.TWILIO_SMS_AUTH_TOKEN) {
-			this.TWILIO_SMS_AUTH_TOKEN = process.env.TWILIO_SMS_AUTH_TOKEN;
-		}
+		this.TWILIO_SMS_AUTH_TOKEN = this.getValueFromOptionsOrEnv(options, 'TWILIO_SMS_AUTH_TOKEN', this.TWILIO_SMS_AUTH_TOKEN);
 
-		if (options.TWILIO_SENDGRID_API_KEY) {
-			this.TWILIO_SENDGRID_API_KEY = this.cleanValue(options.TWILIO_SENDGRID_API_KEY);
-		} else if (process.env.TWILIO_SENDGRID_API_KEY) {
-			this.TWILIO_SENDGRID_API_KEY = process.env.TWILIO_SENDGRID_API_KEY;
-		}
+		this.TWILIO_SENDGRID_API_KEY = this.getValueFromOptionsOrEnv(options, 'TWILIO_SENDGRID_API_KEY', this.TWILIO_SENDGRID_API_KEY);
 
-		if (options.AWS_SES_REGION) {
-			this.AWS_SES_REGION = this.cleanValue(options.AWS_SES_REGION);
-		} else if (process.env.AWS_SES_REGION) {
-			this.AWS_SES_REGION = process.env.AWS_SES_REGION;
-		}
+		this.AWS_SES_REGION = this.getValueFromOptionsOrEnv(options, 'AWS_SES_REGION', this.AWS_SES_REGION);
+
+		this.AWS_SMS_REGION = this.getValueFromOptionsOrEnv(options, 'AWS_SMS_REGION', this.AWS_SMS_REGION);
 	}
 
-	private cleanValue(value: string) {
-		return value.toString().trim();
+	private cleanValue(value?: string) {
+		if (value) {
+			return value.toString().trim();
+		}
+
+		return '';
+	}
+
+	private getValueFromOptionsOrEnv(options: any, env: string, defaultValue: string): string {
+		if (options[env]) {
+			return this.cleanValue(options[env]);
+		}
+
+		if (process.env[env]) {
+			return this.cleanValue(process.env[env]);
+		}
+
+		return defaultValue;
 	}
 }
