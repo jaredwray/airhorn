0466ecd50ff6e9e2068f7fc648183a21a14ad002
Jared Wray <me@jaredwray.com>
2022-07-30 15:04:35 -0700

    Update testing-data.test.ts

--- test/testing-data.test.ts
+++ test/testing-data.test.ts
@@ -1,22 +1,28 @@
 /* eslint-disable n/prefer-global/process */
 import {getFirebaseCert, getSendgridAPIKey} from './testing-data.js';
 
+class NoErrorThrownError extends Error {}
+
+const getError = <TError>(call: () => unknown): TError => {
+	try {
+		call();
+
+		throw new NoErrorThrownError();
+	} catch (error: unknown) {
+		return error as TError;
+	}
+};
+
 test('getFirebaseCert throws an error', () => {
 	delete process.env.FIREBASE_CERT;
+	const error = getError<Error>(getFirebaseCert);
 
-	try {
-		getFirebaseCert();
-	} catch (error) {
-		expect(error).toEqual(new Error('FIREBASE_CERT not defined. Please refer to the README.md under Testing Integrations.'));
-	}
+	expect(error).toEqual(new Error('FIREBASE_CERT not defined. Please refer to the README.md under Testing Integrations.'));
 });
 
 test('getSendgridAPIKey throws an error', () => {
 	delete process.env.TWILIO_SENDGRID_API_KEY;
+	const error = getError<Error>(getSendgridAPIKey);
 
-	try {
-		getSendgridAPIKey();
-	} catch (error) {
-		expect(error).toEqual(new Error('TWILIO_SENDGRID_API_KEY not defined. Please refer to the README.md under Testing Integrations.'));
-	}
+	expect(error).toEqual(new Error('TWILIO_SENDGRID_API_KEY not defined. Please refer to the README.md under Testing Integrations.'));
 });
