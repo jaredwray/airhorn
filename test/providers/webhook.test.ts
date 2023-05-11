27359d0009d40391a75dd8e80334594804f51a77
Jared Wray <me@jaredwray.com>
2023-05-11 10:15:52 -0700

    removing test that is not needed

--- test/providers/webhook.test.ts
+++ test/providers/webhook.test.ts
@@ -3,10 +3,3 @@ import {WebHook} from '../../src/providers/webhook.js';
 test('WebHook - Init', () => {
 	expect(new WebHook()).toEqual(new WebHook());
 });
-
-test('WebHook - Send', async () => {
-	const webhook = new WebHook();
-	const messageData = { answer: 42 };
-	const message = JSON.stringify(messageData);
-	expect(await webhook.send('https://httpbin.org/post', '', message)).toEqual(true);
-});
