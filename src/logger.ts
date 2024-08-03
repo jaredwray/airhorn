16707de3e6e7f8c0e0c00b35a3ab7c3ab77769b7
Jared Wray <me@jaredwray.com>
2024-08-03 12:07:07 -0700

    removing winston for standard logging

--- src/logger.ts
+++ src/logger.ts
@@ -1,12 +1,31 @@
-import * as winston from 'winston';
-import {Options} from './options.js';
+import process from 'node:process';
 
-export function create() {
-	const log = winston.createLogger({transports: [new winston.transports.Console()]});
-	const options = new Options();
-	if (options.ENVIRONMENT === 'test') {
-		log.silent = true;
+export class Logger {
+	silent = false;
+
+	constructor() {
+		if (process.env.NODE_ENV === 'test') {
+			this.silent = true;
+		}
 	}
 
-	return log;
+	public error(message: string) {
+		if (!this.silent) {
+			console.error(message);
+		}
+	}
+
+	public warn(message: string) {
+		if (!this.silent) {
+			console.warn(message);
+		}
+	}
+
+	public info(message: string) {
+		if (!this.silent) {
+			console.info(message);
+		}
+	}
 }
+
+export const log = new Logger();
